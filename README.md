2bd438f6a0adb2b2426da3c5335a507fdd1558ba
tomekp <44640274+tomekpajak@users.noreply.github.com>
2019-11-05 23:30:42 +0100

    add adapter pattern

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# csharp-design-patterns
+# Design patterns using c#
+This is my sandbox for design patterns in c#
+
+- Adapter
+
+## Design patterns
+### Adapter
+Umożliwia wykorzystanie klasy A implementującej interfejs A przez klienta, który oczekuje interfejsu B.\
+W tym celu tworzy się klasę AdapterA implementującą imterfejs B, gdzie poszczególne metody interfejsu B są delegowane do metod interfejsu A.
+
+Jednym z zastosowań wzorca adapter jest zamiana zależności zewnętrznych na zależności wewnętrzne.\
+Np. w usłudze mamy zależność od zewnętrznej biblioteki logującej. 
+Jeśli utworzymy wewnętrzny interfejs ILogger oraz stworzymy adapter zewnętrznej biblioteki implementujący interfejs ILogger zmienimy zewnętrzną zależność na zależność wewnętrzną.
---
+++ tp.Adapter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace tp.Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new XMLConfigurationAdapter(new XMLConfigurationExternalService());
+            var value = configuration.GetValue("host-name");
+
+            Console.WriteLine(value);
+        }
+    }
+
+    interface IConfiguration
+    {
+        string GetValue(string name);
+    }
+
+    class XMLConfigurationAdapter : IConfiguration
+    {
+        private readonly IXMLConfiguration xmlConfiguration;
+        public XMLConfigurationAdapter(IXMLConfiguration xmlConfiguration)
+        {
+            this.xmlConfiguration = xmlConfiguration;
+        }
+        public string GetValue(string name) => this.xmlConfiguration.GetAttributeValue(name);
+    }
+
+    interface IXMLConfiguration
+    {
+        string GetAttributeValue(string name);
+    }
+
+    class XMLConfigurationExternalService : IXMLConfiguration
+    {
+        public string GetAttributeValue(string name) => "fake-host";
+    }
+}
